2ab09e49fef3ea2054a4692ed649922033307e93
Steffen Borchmann <steffen.borchmann@tualo.de>
2025-07-04 10:35:36 +0200

    add security helper for static file serving

--- src/Routes/JsLoader.php
+++ src/Routes/JsLoader.php
@@ -4,14 +4,16 @@ namespace Tualo\Office\UserEditor\Routes;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
-
+use Tualo\Office\Basic\RouteSecurityHelper; 
 class JsLoader implements IRoute{
     public static function register(){
         BasicRoute::add('/jsusereditor/(?P<file>[\w.\/\-]+).js',function($matches){
-            App::contenttype('application/javascript');
-            readfile( dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js' );
-            exit();
-            
+            RouteSecurityHelper::serveSecureStaticFile(
+                $matches['file'] . '.js',
+                dirname(__DIR__, 1) . '/js/lazy/',
+                ['js'],
+                ['application/javascript']
+            );
         },['get'],false);
 
     }
